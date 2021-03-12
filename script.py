ffe04cfc6d33226265a69bfcfb185589a1c160b5
AmitLaloush <amitlaloush@gmail.com>
2021-03-12 10:26:35 +0200

    Add files via upload

---
+++ script.py
@@ -0,0 +1,18 @@
+f = open("file.txt", "r")
+fileContent = f.read().split("\n")
+
+counter = 0
+final_result = ""
+for line in fileContent:
+    leadind_string = "<string name=\"notification_body_morning"+ str(counter) + "\">"
+    ending_string = "</string>"
+    filtered_line = line.lstrip()[3:].lstrip()
+    
+    line_result = leadind_string + filtered_line + ending_string
+    final_result += line_result + "\n"
+    counter+=1
+
+
+
+f = open("result.txt", "a")
+f.write(final_result)
